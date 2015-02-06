5caf23ec5e300b1b9fe7f81b76da289c14b2dd70
John DeHerrera <jdeherrera@gmail.com>
2015-02-06 09:21:05 -0600

    Renamed function

--- enum.js
+++ enum.js
@@ -11,7 +11,7 @@
  *
  * @return {object}
  */
-var toEnum = function(/* arguments */) {
+var enum = function(/* arguments */) {
     var obj = {};
     var args = Array.prototype.slice.call(arguments, 1);
     var len = args.length;
