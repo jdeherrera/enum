435c3478cc3f323ef128862869f5d9c2608262e8
John DeHerrera <john.deherrera@akunacapital.com>
2015-02-06 09:20:10 -0600

    The _real_ first commit

---
+++ enum.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Constructs an enumeration with keys equal to their value.
+ *
+ * For example:
+ *
+ *   var COLORS = toEnum('blue', 'red');
+ *   var myColor = COLORS.blue;
+ *   var isColorValid = !!COLORS[myColor];
+ *
+ * @return {object}
+ */
+var toEnum = function(/* arguments */) {
+    var obj = {};
+    var args = Array.prototype.slice.call(arguments, 1);
+    var len = args.length;
+    var i, key;
+
+    for (i = 0; i < len; i++) {
+        key = args[i];
+        obj[key] = key;
+    }
+
+    return obj;
+};
+
+module.exports = toEnum;
